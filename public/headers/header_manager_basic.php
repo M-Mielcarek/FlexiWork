0339c64f3e298655bbed94e0ae58f7664e13d874
M-Mielcarek <malgorzata.m.mielcarek@gmail.com>
2026-05-06 14:56:14 +0200

    Add files via upload

---
+++ public/headers/header_manager_basic.php
@@ -0,0 +1,17 @@
+<header>
+    <ul class="nav">
+        <li><a href="<?php echo BASE_URL . 'public/profil_uzytkownika.php'; ?>">Profil użytkownika</a></li>
+
+        <li><a href="<?php echo BASE_URL . 'public/lista_uzytkownikow.php'; ?>">Lista pracowników</a></li>
+
+        <li><a href="<?php echo BASE_URL . 'resources/views//schedule/ustalanie_grafiku.php'; ?>">Ustalanie grafiku</a></li>
+
+        <li><a href="<?php echo BASE_URL . 'announcements/tablica_ogloszen.php'; ?>">Tablica Ogłoszeń</a></li>
+
+        <li><a href="<?php echo BASE_URL . 'announcements/bank_wymian.php'; ?>">Bank Wymian</a></li>
+
+        <li><a href="<?php echo BASE_URL . 'resources/views/auth/logout.php'; ?>">Wyloguj</a></li>
+
+    </ul>
+
+</header>
---
+++ public/headers/header_worker_basic.php
@@ -0,0 +1,17 @@
+<header>
+    <ul class="nav">
+        <li><a href="<?php echo BASE_URL . 'public/profil_uzytkownika.php'; ?>">Profil użytkownika</a></li>
+
+        <li><a href="<?php echo BASE_URL . 'resources/views//schedule/grafik_uzytkownika.php'; ?>">Grafik</a></li>
+
+        <li><a href="<?php echo BASE_URL . 'public/raport_uzytkownika.php'; ?>">Raport</a></li>
+
+        <li><a href="<?php echo BASE_URL . 'announcements/tablica_ogloszen.php'; ?>">Tablica Ogłoszeń</a></li>
+
+        <li><a href="<?php echo BASE_URL . 'announcements/bank_wymian.php'; ?>">Bank Wymian</a></li>
+
+        <li><a href="<?php echo BASE_URL . 'resources/views/auth/logout.php'; ?>">Wyloguj</a></li>
+
+    </ul>
+
+</header>
